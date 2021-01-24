dcd886798cb681d8d574f2a18aa35dd015d5cb95
murphy-warugaki <murphydayo120@gmail.com>
2021-01-25 04:58:03 +0900

    comp

---
+++ app/Http/Controllers/Service/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Service;
+
+use Auth;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function create(Request $request)
+    {
+
+    }
+}
